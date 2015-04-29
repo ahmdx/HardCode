cb5c7e25dda30dccc9d85ee4d7dddeed5cd0e678
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2015-04-29 21:42:49 +0300

    Shell progress

--- M4/shell.c
+++ M4/shell.c
@@ -2,6 +2,11 @@ void handleCommand(char*);
 
 int main() {
   char line[100];
+  char directory[512];
+  char dirName[32];
+  int dirIndex = 0;
+  int dirEntry = 0;
+  int dirSector = 0;
   //while(1) {
   // makeInterrupt21();
   interrupt(0x21, 0, "shell$ ", 0, 0);
@@ -13,9 +18,13 @@ int main() {
 
 void handleCommand(char* input) {
   char directory[512];
-  char dirName[9];
+  char dirName[32];
+  char fileName[32];
+  char lineRead[100];
+  char file[13312];
   int dirIndex = 0;
   int dirEntry = 0;
+  int dirSector = 1;
   //   int i = 0;
   //   int comm = 0;
   //   char* view = "view";
@@ -33,25 +42,60 @@ void handleCommand(char* input) {
 	interrupt(0x21, 0, "delete", 0, 0);
   } else if (input[0] == 'c' && input[1] == 'o' && input[2] == 'p' && input[3] == 'y') {
 	interrupt(0x21, 0, "copy", 0, 0);
-  } else if (input[0] == 'd' && input[1] == 'i' && input[2] == 'r') {
+  }
+  else if (input[0] == 'd' && input[1] == 'i' && input[2] == 'r') {
 	interrupt(0x21, 2, directory, 2, 0);
 	while (dirIndex < 16) {
 	  if (directory[dirIndex*32] != 0x00) {
 		dirEntry = 0;
 		while(dirEntry < 6) {
-		  dirName[dirEntry] = directory[dirIndex*32 + dirEntry];
+		  if (directory[dirIndex*32 + dirEntry] != 0x00) {
+			dirName[dirEntry] = directory[dirIndex*32 + dirEntry];
+		  } else {
+			dirName[dirEntry] = 0x5F;
+		  }
 		  dirEntry++;
 		}
-		dirName[dirEntry] = '\r';
+		dirEntry = 6;
+		while(dirEntry < 32) {
+		  if (directory[dirIndex*32 + dirEntry] == 0x00) {
+			break;
+		  }
+		  dirSector++;
+		  dirEntry++;
+		}
+// 		interrupt(0x21, 0, 2 + '0', 0, 0);
+		dirEntry = 6;
+		dirName[dirEntry + 0] = 1 + '0';
+		dirName[dirEntry + 0] = '\r';
 		dirName[dirEntry + 1] = '\n';
-		interrupt(0x21, 0, dirName, 0, 0);s
+		dirName[dirEntry + 2] = 0x00;
+		interrupt(0x21, 0, dirName, 0, 0);
+		dirSector = 0;
 	  }
 	  dirIndex++;
 	}
-	//   readSector(directory, 2);
-  } else if (input[0] == 'c' && input[1] == 'r' && input[2] == 'e' && input[3] == 'a' && input[4] == 't' && input[5] == 'e') {
-	interrupt(0x21, 0, "create", 0, 0);
-  } else {
+  }
+  //   else if (input[0] == 'c' && input[1] == 'r' && input[2] == 'e' && input[3] == 'a' && input[4] == 't' && input[5] == 'e') {
+  // 	while(input[dirIndex + 7] != '\0') {
+  // 	  fileName[dirIndex] = input[dirIndex + 7];
+  // 	  dirIndex++;
+  // 	}
+  // 	interrupt(0x21, 0, "create> ",0, 0);
+  // 	interrupt(0x21, 1, lineRead, 0, 0);
+  // 	while(lineRead[0] != 0xd) {
+  // 	  dirIndex = 0;
+  // 	  while(lineRead[dirIndex] != '\0') {
+  // 		file[dirEntry] = lineRead[dirIndex];
+  // 		dirIndex++;
+  // 		dirEntry++;
+  // 	  }
+  // 	  interrupt(0x21, 0, "create> ",0, 0);
+  // 	  interrupt(0x21, 1, lineRead, 0, 0);
+  // 	}
+  // 	interrupt(0x21, 8, fileName, file, 1);
+  //   }
+  else {
 	interrupt(0x21, 0, "Fatal: Bad command - ", 0, 0);
 	interrupt(0x21, 0, input, 0, 0);
   }
